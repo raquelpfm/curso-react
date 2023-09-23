1502efdb8280ae70b313578506cfbd7dbf48469c
Raquel <raquelparadella@gmail.com>
2023-09-23 09:11:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, {Component} from 'react';
+import Table from './table';
+import Form from './form';
+
+class App extends Component {
+  state = {
+    alunos: []
+  }
+  removerAluno = (index) => {
+    const{alunos} = this.state
+    this.setState({
+      alunos: alunos.filter((aluni, i) => {
+        return i !== index
+      })
+    })
+  }
+  handleSubmit = (alunoDisc) => {
+    this.setState({
+        alunos: [...this.state.alunos, alunoDisc]
+    })
+}
+  render() {
+    const {alunos} = this.state
+    return (
+      <div className='container'>
+        <Table alunos = {alunos}
+          removerAluno={this.removerAluno} />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
+  }
+}
+
+export default App;
